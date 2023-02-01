d4f83ef0da7f04e14ad36fb0e3d88c167e99d71c
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-02-01 15:42:55 -0500

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+let timerId = null;
+
+function startChangingColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+
+function stopChangingColor() {
+ startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+}
+
+startBtn.addEventListener('click', startChangingColor);
+stopBtn.addEventListener('click', stopChangingColor);
+
